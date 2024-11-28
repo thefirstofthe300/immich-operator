c5b32401607f431260b4bb6e9483251c7efdbecf
Danny Seymour <danny@seymour.family>
2024-11-27 23:16:02 -0800

    feat: Add initial dev shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          packages = [
+            go
+            operator-sdk
+          ];
+        };
+      }
+    );
+}
